c9e8c26c187db4c87f78565e835e6b63e3d184d9
Sean Marquez <capsulecorplab@gmail.com>
2022-10-12 02:48:41 +0000

    revise fuseki_cleaner to use argparse
    - require input argument as flag value, using -i or --input
    - add optional output argument as flag value, using -o or --output
    - print help text if no arguments passed

--- fuseki_cleaner.py
+++ fuseki_cleaner.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 # script developed with help from https://stackoverflow.com/questions/79968/split-a-string-by-spaces-preserving-quoted-substrings-in-python
 
+import argparse
+import sys
 import shlex
 
-with open("data.ttl", "r") as file:
+parser = argparse.ArgumentParser(description="Convert quadruples to triples.")
+parser.add_argument(
+    "-i", "--input", type=str, nargs=1, help="Filename of TTL to be cleaned"
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    type=str,
+    nargs="?",
+    default="new_data.ttl",
+    help="Filename of output TTL",
+)
+
+if len(sys.argv) == 1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+
+args = parser.parse_args()
+
+with open(args.input[0], "r") as file:
     data = file.read()
 
 new_data = ""
@@ -15,5 +36,5 @@ for line in data.split("\n"):
         )
         new_data = new_data + new_line + "\n"
 
-with open("new_data.ttl", "w") as new_file:
+with open(args.output, "w") as new_file:
     new_file.write(new_data)
