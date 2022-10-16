0c6e9f5c85a7e085d9a6cade76e710e647cad35b
Sean Marquez <capsulecorplab@gmail.com>
2022-10-15 18:07:45 -0600

    add verbose flag for printing parsed arguments

--- fuseki_cleaner.py
+++ fuseki_cleaner.py
@@ -18,11 +18,18 @@ parser.add_argument(
     help="Filename of output TTL",
 )
 
+parser.add_argument(
+    "-v", "--verbose", help="increase output verbosity", action="store_true"
+)
+
 if len(sys.argv) == 1:
     parser.print_help(sys.stderr)
     sys.exit(1)
 
 args = parser.parse_args()
+
+if args.verbose:
+    print(args)
 
 with open(args.input[0], "r") as file:
     data = file.read()
